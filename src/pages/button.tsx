5139221076859287d17a378560c2709e3fd28fa8
yinruo <yinruoh@gmail.com>
2023-05-17 14:57:59 +0200

    progress of sorting and filters

--- src/pages/button.tsx
+++ src/pages/button.tsx
@@ -1,71 +1,76 @@
-/* import { useState } from "react";
+import { useState } from 'react'
 
 interface Option {
-  value: string;
-  label: string;
-  subOptions: string[];
+  id: number
+  value: string
+  label: string
+  subOptions: string[]
 }
 
 const options: Option[] = [
   {
-    value: "name",
-    label: "Name",
-    subOptions: ["apple 1", "apple 2"],
+    id: 1,
+    value: 'apple',
+    label: 'Apple',
+    subOptions: ['apple 1', 'apple 2']
   },
   {
-    value: "pear",
-    label: "Pear",
-    subOptions: ["pear 1", "pear 2"],
+    id: 2,
+    value: 'pear',
+    label: 'Pear',
+    subOptions: ['pear 1', 'pear 2']
   },
   {
-    value: "peach",
-    label: "Peach",
-    subOptions: ["peach 1", "peach 2"],
-  },
-];
+    id: 3,
+    value: 'peach',
+    label: 'Peach',
+    subOptions: ['peach 1', 'peach 2']
+  }
+]
 
 const App = () => {
-  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([])
 
   const handleAddFilter = () => {
-    setSelectedOptions((prevSelectedOptions) => [
-      ...prevSelectedOptions,
-      "",
-      "",
-    ]);
-  };
+    setSelectedOptions((prevSelectedOptions) => [...prevSelectedOptions, '', ''])
+  }
 
   const handleOptionChange = (index: number, value: string) => {
     setSelectedOptions((prevSelectedOptions) => {
-      const newSelectedOptions = [...prevSelectedOptions];
-      newSelectedOptions[index] = value;
-      return newSelectedOptions;
-    });
-  };
+      const newSelectedOptions = [...prevSelectedOptions]
+      newSelectedOptions[index] = value
+      return newSelectedOptions
+    })
+  }
 
   const handleSubmit = () => {
-    const result: string[] = [];
+    const result: string[] = []
     for (let i = 0; i < selectedOptions.length; i += 2) {
-      const option = selectedOptions[i];
-      const subOption = selectedOptions[i + 1];
+      const option = selectedOptions[i]
+      const subOption = selectedOptions[i + 1]
       if (option && subOption) {
-        result.push(`${option}: ${subOption}`);
+        result.push(`${option}: ${subOption}`)
       }
     }
-    alert(result.join(", "));
-  };
+    alert(result.join(', '))
+  }
 
   return (
     <div>
       <h1>Add Filters</h1>
-      <button onClick={handleAddFilter}>Add Filter</button>
+      <button type="button" onClick={handleAddFilter}>
+        Add Filter
+      </button>
       {selectedOptions.map((selectedOption, index) => {
-        const isSecondSelect = index % 2 === 1;
-        const parentSelectIndex = Math.floor(index / 2);
-        const parentSelectValue = selectedOptions[parentSelectIndex * 2];
+        // whether the select element is the second one or not. Parent select option is index/2. EveryOption, including
+        // suboptions has a unique number.
+        const isSecondSelect = index % 2 === 1
+        const parentSelectIndex = Math.floor(index / 2)
+        const parentSelectValue = selectedOptions[parentSelectIndex * 2]
+        const key = isSecondSelect ? `second_${index}` : `first_${index}`
 
         return (
-          <div key={index}>
+          <div key={key}>
             {isSecondSelect && parentSelectValue && (
               <select
                 value={selectedOption}
@@ -88,18 +93,20 @@ const App = () => {
               >
                 <option value="">Select an option</option>
                 {options.map((option) => (
-                  <option key={option.value} value={option.value}>
+                  <option key={option.id} value={option.value}>
                     {option.label}
                   </option>
                 ))}
               </select>
             )}
           </div>
-        );
+        )
       })}
-      <button onClick={handleSubmit}>Submit</button>
+      <button type="button" onClick={handleSubmit}>
+        Submit
+      </button>
     </div>
-  );
-};
+  )
+}
 
-export default App; */
+export default App
